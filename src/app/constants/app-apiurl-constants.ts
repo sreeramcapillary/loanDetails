f1fa163e5f8ab57ac67984101a903e9b4dcf394f
sreeram.r <sreeram.prasad@capillarytech.com>
2019-10-16 23:08:38 +0530

    Bug fixes

--- src/app/constants/app-apiurl-constants.ts
+++ src/app/constants/app-apiurl-constants.ts
@@ -1,5 +1,5 @@
 export const BaseURL = {
-    //url: 'http://localhost:3000/', //Local
+    // url: 'http://localhost:3000/', //Local
      url: 'http://148.72.212.163/backend/',  //Live
 };
 export const API = {
--- src/app/modules/xlupload/xlupload/xlupload.component.ts
+++ src/app/modules/xlupload/xlupload/xlupload.component.ts
@@ -6,7 +6,8 @@ import { HttpClient } from '@angular/common/http';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
 declare var $;
-const URL = 'http://localhost:3000/uploadExcel';
+// const URL = 'http://localhost:3000/uploadExcel'; // Local
+const URL = 'http://148.72.212.163/uploadExcel'; // Live
 
 @Component({
   selector: 'app-xlupload',
@@ -19,7 +20,7 @@ export class XluploadComponent implements OnInit {
   public uploader: FileUploader = new FileUploader({ url: URL, itemAlias: 'leadExcel' });
   xluploaddata: any;
   emplist: any[];
-  loanDetails: any;
+  loanDetails: any[];
   bucketList: any;
   languageList: any;
   usersWithKnownLanguages: any[];
@@ -50,49 +51,85 @@ export class XluploadComponent implements OnInit {
           this.appService.xlupload(this.xluploaddata).subscribe(
             (data: any) => {
               if (data.status) {
-                this.loanDetails = [];
-
+                this.loanDetails = []
                 this.loanDetails['bucket'] = [];
-                for (var i = 0; i < this.xluploaddata.data.length; i++) {
-                  var bucket = this.xluploaddata.data[i].bucket;
-                  if (bucket in this.loanDetails == false) {
-                    this.loanDetails['bucket'][bucket] = []; // must initialize the sub-object, otherwise will get 'undefined' errors
+                //Making bucket array
+                this.xluploaddata.data.map((row,key) => {
+                  if(!this.loanDetails["bucket"].includes(row.bucket)){
+                    this.loanDetails["bucket"][row.bucket] = [];
+                    this.loanDetails['bucket'][row.bucket]['language'] = [];
                   }
-                  this.loanDetails['bucket'][bucket]['langauage'] = [];
-                  for (var j = 0; j < this.xluploaddata.data.length; j++) {
-                    if (this.xluploaddata.data[i].bucket == this.xluploaddata.data[j].bucket) {
-                      var state = this.xluploaddata.data[j].state.toLowerCase();
-                      if (state in this.loanDetails == false) {
-                        this.loanDetails['bucket'][bucket]['langauage'][state] = []; // must initialize the sub-object, otherwise will get 'undefined' errors
+                })
+                //Making language array
+                this.xluploaddata.data.map((row,key) => {
+                  if(!this.loanDetails["bucket"][row.bucket]["language"].includes(row.state.toLowerCase())){
+                    this.loanDetails['bucket'][row.bucket]['language'][row.state.toLowerCase()] = [];
+                    this.loanDetails['bucket'][row.bucket]['language'][row.state.toLowerCase()]["loanData"] = [];
+                    this.loanDetails['bucket'][row.bucket]['language'][row.state.toLowerCase()]["empdetails"] = [];
+                    this.loanDetails['bucket'][row.bucket]['language'][row.state.toLowerCase()]["totalLoanDataValue"] = 0;
+                  }
+                })
+                //Pushing the loan details to respective bucket,langugage,state and employee details
+                this.xluploaddata.data.map((row,key) => {
+                  row.is_assigned = false
+                  this.loanDetails['bucket'][row.bucket]['language'][row.state.toLowerCase()]["loanData"].push(row)
+                  this.loanDetails['bucket'][row.bucket]['language'][row.state.toLowerCase()]["totalLoanDataValue"] = this.loanDetails['bucket'][row.bucket]['language'][row.state.toLowerCase()]["totalLoanDataValue"] + parseInt(row.repayment_amt)
+
+
+                  this.emplist.map(elist => {
+                    if (elist.state_name.toLowerCase() == row.state.toLowerCase() && elist.bucket == row.bucket) {
+                      var empdata = {
+                        "empid": elist.id,
+                        "assigned" : false
                       }
-                      var loandatadetails = []
-                      var empdetails = []
-                      var totalLoanDataValue = 0;
-                      for (var k = 0; k < this.xluploaddata.data.length; k++) {
-                        if (this.xluploaddata.data[j].bucket == this.xluploaddata.data[k].bucket && this.xluploaddata.data[j].state.toLowerCase() == this.xluploaddata.data[k].state.toLowerCase()) {
-                          totalLoanDataValue = totalLoanDataValue + parseInt(this.xluploaddata.data[k].repayment_amt);
-                          this.xluploaddata.data[k].is_assigned = false;
-                          loandatadetails.push(this.xluploaddata.data[k])
-                        }
-                      }
-                      this.emplist.map(elist => {
-                        if (elist.state_name.toLowerCase() == this.xluploaddata.data[j].state.toLowerCase() && elist.bucket == this.xluploaddata.data[j].bucket) {
-                          var empdata = {
-                            "empid": elist.id,
-                            "assigned" : false
-                          }
-                          empdetails.push(empdata);
-                        }
-                      })
-                      this.loanDetails['bucket'][bucket]['langauage'][state]['loanData'] = loandatadetails;
-                      this.loanDetails['bucket'][bucket]['langauage'][state]['empdetails'] = empdetails;
-                      this.loanDetails['bucket'][bucket]['langauage'][state]['totalLoanDataValue'] = totalLoanDataValue;
+                      this.loanDetails['bucket'][row.bucket]['language'][row.state.toLowerCase()]["empdetails"].push(empdata);
                     }
+                  })
+                })
 
-                  }
+                // console.log("bucket array", this.loanDetails)
+                // this.loanDetails = [];
+                // this.loanDetails['bucket'] = [];
+                // for (var i = 0; i < this.xluploaddata.data.length; i++) {
+                //   var bucket = this.xluploaddata.data[i].bucket;
+                //   if (bucket in this.loanDetails == false) {
+                //     this.loanDetails['bucket'][bucket] = []; // must initialize the sub-object, otherwise will get 'undefined' errors
+                //   }
+                //   this.loanDetails['bucket'][bucket]['langauage'] = [];
+                //   for (var j = 0; j < this.xluploaddata.data.length; j++) {
+                //     if (this.xluploaddata.data[i].bucket == this.xluploaddata.data[j].bucket) {
+                //       var state = this.xluploaddata.data[j].state.toLowerCase();
+                //       if (state in this.loanDetails == false) {
+                //         this.loanDetails['bucket'][bucket]['langauage'][state] = []; // must initialize the sub-object, otherwise will get 'undefined' errors
+                //       }
+                //       var loandatadetails = []
+                //       var empdetails = []
+                //       var totalLoanDataValue = 0;
+                //       for (var k = 0; k < this.xluploaddata.data.length; k++) {
+                //         if (this.xluploaddata.data[j].bucket == this.xluploaddata.data[k].bucket && this.xluploaddata.data[j].state.toLowerCase() == this.xluploaddata.data[k].state.toLowerCase()) {
+                //           totalLoanDataValue = totalLoanDataValue + parseInt(this.xluploaddata.data[k].repayment_amt);
+                //           this.xluploaddata.data[k].is_assigned = false;
+                //           loandatadetails.push(this.xluploaddata.data[k])
+                //         }
+                //       }
+                //       this.emplist.map(elist => {
+                //         if (elist.state_name.toLowerCase() == this.xluploaddata.data[j].state.toLowerCase() && elist.bucket == this.xluploaddata.data[j].bucket) {
+                //           var empdata = {
+                //             "empid": elist.id,
+                //             "assigned" : false
+                //           }
+                //           empdetails.push(empdata);
+                //         }
+                //       })
+                //       this.loanDetails['bucket'][bucket]['langauage'][state]['loanData'] = loandatadetails;
+                //       this.loanDetails['bucket'][bucket]['langauage'][state]['empdetails'] = empdetails;
+                //       this.loanDetails['bucket'][bucket]['langauage'][state]['totalLoanDataValue'] = totalLoanDataValue;
+                //     }
 
-                }
-             //   console.log("Loan Details Filetered", this.loanDetails);
+                //   }
+
+                // }
+               console.log("Loan Details Filetered", this.loanDetails);
 
                 //Getting unique employee array and setting assigned loans count to 0 
                 let uniqueEmployeeList = []
@@ -106,19 +143,19 @@ export class XluploadComponent implements OnInit {
                 var finalFilteredArray = [];
 
                 for (var bucketKey in this.loanDetails['bucket']) {
-                  for (var languageKey in this.loanDetails['bucket'][bucketKey]["langauage"]) {
-                    var employeeCount = this.loanDetails['bucket'][bucketKey]["langauage"][languageKey]["empdetails"].length;
+                  for (var languageKey in this.loanDetails['bucket'][bucketKey]["language"]) {
+                    var employeeCount = this.loanDetails['bucket'][bucketKey]["language"][languageKey]["empdetails"].length;
                     // console.log("employeeCount for bucket "+bucketKey+" => Language "+languageKey+" : ",employeeCount)
                     if (employeeCount > 0) {
-                      var averageLoanValue = (parseInt(this.loanDetails['bucket'][bucketKey]["langauage"][languageKey]["totalLoanDataValue"], 10) / employeeCount);
-                      console.log("averageLoanValue for bucket "+bucketKey+" => Language "+languageKey+" : "+averageLoanValue+", totalLoanDataValue : "+parseInt(this.loanDetails['bucket'][bucketKey]["langauage"][languageKey]["totalLoanDataValue"], 10))
-                      // console.log(this.loanDetails['bucket'][bucketKey]["langauage"][languageKey]["empdetails"])
+                      var averageLoanValue = (parseInt(this.loanDetails['bucket'][bucketKey]["language"][languageKey]["totalLoanDataValue"], 10) / employeeCount);
+                      console.log("averageLoanValue for bucket "+bucketKey+" => Language "+languageKey+" : "+averageLoanValue+", totalLoanDataValue : "+parseInt(this.loanDetails['bucket'][bucketKey]["language"][languageKey]["totalLoanDataValue"], 10))
+                      // console.log(this.loanDetails['bucket'][bucketKey]["language"][languageKey]["empdetails"])
                       var ldata;
                       var aempid
 
-                      this.loanDetails['bucket'][bucketKey]["langauage"][languageKey]["empdetails"].map(emp => {
+                      this.loanDetails['bucket'][bucketKey]["language"][languageKey]["empdetails"].map(emp => {
                         let totalAmountAssigned = 0
-                        this.loanDetails['bucket'][bucketKey]["langauage"][languageKey]["loanData"].map(data => {
+                        this.loanDetails['bucket'][bucketKey]["language"][languageKey]["loanData"].map(data => {
                           if (uniqueEmployeeList[emp.empid] <= 120 && totalAmountAssigned <= averageLoanValue && data.is_assigned == false){
                             totalAmountAssigned = (totalAmountAssigned+parseInt(data.repayment_amt, 10))
                             //Incrementing count of loans assigned to individual employee
@@ -133,8 +170,8 @@ export class XluploadComponent implements OnInit {
                           }
                         })
                       })
-                      // this.loanDetails['bucket'][bucketKey]["langauage"][languageKey]["loanData"].map(data => {
-                      //   this.loanDetails['bucket'][bucketKey]["langauage"][languageKey]["empdetails"].map(emp => {
+                      // this.loanDetails['bucket'][bucketKey]["language"][languageKey]["loanData"].map(data => {
+                      //   this.loanDetails['bucket'][bucketKey]["language"][languageKey]["empdetails"].map(emp => {
                       //     if (averageLoanValue <= data.repayment_amt && data.is_assigned == false && aempid != emp.empid) {
                       //       aempid = emp.empid;
                       //       data["is_assigned"] = true;
@@ -151,7 +188,8 @@ export class XluploadComponent implements OnInit {
                   }
                 }
                 //console.log("uniqueEmployeeList", uniqueEmployeeList)
-               // console.log("finalFilteredArray", finalFilteredArray)
+               console.log("finalFilteredArray", finalFilteredArray)
+              //  return false
                 if (finalFilteredArray) {
                   this.appService.updateLoanData(finalFilteredArray).subscribe(
                     (data: any) => {

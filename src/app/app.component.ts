42ec3c2703c58b729eea25fc1b91e91c803db886
sreeram.r <sreeram.prasad@capillarytech.com>
2020-04-27 03:38:19 +0530

    removed loan count of emp, lead, added call done in emp loans list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,30 +14,30 @@ export class AppComponent {
   }
 
   ngOnInit(){
-      setInterval(() => {
-        if(localStorage.getItem('usertype') == "2" || localStorage.getItem('usertype') == "0"){
-          var date1, date2;  
+      // setInterval(() => {
+      //   if(localStorage.getItem('usertype') == "2" || localStorage.getItem('usertype') == "0"){
+      //     var date1, date2;  
       
-          date1 = new Date( localStorage.getItem('loggedInTimeStamp') );
-          console.log(date1);
+      //     date1 = new Date( localStorage.getItem('loggedInTimeStamp') );
+      //     console.log(date1);
       
-          date2 = new Date();
-          console.log(date2);
+      //     date2 = new Date();
+      //     console.log(date2);
       
-          var res = Math.abs(date1 - date2) / 1000;
+      //     var res = Math.abs(date1 - date2) / 1000;
     
-          var minutes = Math.floor(res / 60) % 60;
-          console.log("Difference (Minutes): "+minutes);  
+      //     var minutes = Math.floor(res / 60) % 60;
+      //     console.log("Difference (Minutes): "+minutes);  
     
-          if(minutes >= 20){ 
-            this.appService.logoutAction(localStorage.getItem('userId'))
-            .subscribe(
-            (data:any) => {
-              localStorage.clear();
-              this.router.navigateByUrl('/login');
-            });
-          }
-        }
-      }, 1000*60);
+      //     if(minutes >= 20){ 
+      //       this.appService.logoutAction(localStorage.getItem('userId'))
+      //       .subscribe(
+      //       (data:any) => {
+      //         localStorage.clear();
+      //         this.router.navigateByUrl('/login');
+      //       });
+      //     }
+      //   }
+      // }, 1000*60);
   }
 }
